c406ba3829b8834c55b5f508144b338002208ed9
Unname-addictioner <68629080+Unname-addictioner@users.noreply.github.com>
2020-10-01 14:22:49 +0900

    自動再生機能追加

--- basic_practice/practice/js/script.js
+++ basic_practice/practice/js/script.js
@@ -5,4 +5,10 @@ $(function(){
     $(".btn").on("click",()=>{
         $("#slide").slideToggle();
     })
+    $('autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,    
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
 });
